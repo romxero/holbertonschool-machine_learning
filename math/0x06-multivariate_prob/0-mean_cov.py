b156d0daffb304944cc5e1d6ee1523399fcba739
_RC_ <randy.white.1986@gmail.com>
2021-10-25 09:49:13 -0700

    Update 0-mean_cov.py

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -4,15 +4,27 @@
 
 #camel case notation
 
+
+
+from typing import Sequence
+
+
 def mean_cov(matrix): 
     if matrix is 0:
         return -1 #short circuit
     import numpy as np
+    import math as mat
     npInternalArray = np.array(matrix) #grab the numpy array
     internalMean = np.mean(npInternalArray)
-    #manual calculation of covariant    
-    for i in matrix:
 
-        break
+
+    C = [[]] #make a null matrix 
+
+    for i in range(6):
+        for j in range(6):
+    #manual calculation of covariant possibly can't compute if its not a square matrix
+            C[i][j] = npInternalArray[i][j]/(mat.sqrt(npInternalArray[i][i] * npInternalArray[j][j])) #test 
+    return internalMean, C #aleph null
+
+
     
-    return internalMean, 0 #aleph null
