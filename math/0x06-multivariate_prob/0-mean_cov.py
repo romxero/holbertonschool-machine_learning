859c541b2cee02adcae73f519d0ed2f61da42507
_RC_ <randy.white.1986@gmail.com>
2021-10-25 09:09:07 -0700

    Update 0-mean_cov.py

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
-#test
+#test out multinomial probability density functionm 
+
+#camel case notation
+
+
+
+def mean_cov(matrix): 
+    if matrix is 0:
+        return -1 #short circuit
+    import numpy as np
+    npInternalArray = np.array(matrix) #grab the numpy array
+    internalMean = np.mean(npInternalArray)
+
+
+    #retrun(np.cov(matrix))
+
+    #manual calculation of covariant    
+    for i in matrix:
+
+        break
+    
+    return internalMean, 0 #aleph null
