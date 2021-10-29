0288b775a4da1f23f245fbcd3545386fe75d222d
_RC_ <randy.white.1986@gmail.com>
2021-10-29 16:01:12 -0500

    updating the task

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -20,7 +20,7 @@ class Neuron:
     #make sure to have thre variables here 
 
 
-lib_train = np.load('./data/Binary_Train.npz')
+lib_train = np.load('../data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
 X = X_3D.reshape((X_3D.shape[0], -1)).T
 
