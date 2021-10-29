9a96ddc3b912ac2aa09d4ac68bf5e6bb66cf3b35
_RC_ <randy.white.1986@gmail.com>
2021-10-29 16:44:15 -0500

    Create 1-neuron.py

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class Neuron:
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if (nx) < 1:
+            raise ValueError("nx must be a positive integer")
+        try:
+            self.W = np.random.randn(1, nx)
+            self.b = 0
+            self.A = 0
+            self.nx = nx
+            self.__W = np.random.randn(1, nx)
+            self.__b = 0
+            self.__A = 0
+        except:
+            print("An exception occurred")
+    # make sure to have thre variables here
