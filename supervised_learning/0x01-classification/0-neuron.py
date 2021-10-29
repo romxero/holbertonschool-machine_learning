f35195ae56aca680a1ab2a0a5810e5299f28b861
_RC_ <randy.white.1986@gmail.com>
2021-10-29 16:23:40 -0500

    Update 0-neuron.py

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import numpy as np 
 
 
 class Neuron:
+    import numpy as np 
     def __init__(self,nx):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -20,15 +20,3 @@ class Neuron:
     #make sure to have thre variables here 
 
 
-lib_train = np.load('../data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-print(neuron.W)
-print(neuron.W.shape)
-print(neuron.b)
-print(neuron.A)
-neuron.A = 10
-print(neuron.A)
