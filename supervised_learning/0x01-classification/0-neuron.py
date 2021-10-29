1e40d271bd879781ac236bd3603a85e0e6abea77
_RC_ <randy.white.1986@gmail.com>
2021-10-29 16:24:34 -0500

    Update 0-neuron.py

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import numpy as np 
 
 
 class Neuron:
-    import numpy as np 
     def __init__(self,nx):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
