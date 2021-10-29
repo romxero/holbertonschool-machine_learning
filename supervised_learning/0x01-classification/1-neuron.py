9db56dd5a84ec42e14c9d8b43a933a374def9ee3
_RC_ <randy.white.1986@gmail.com>
2021-10-29 16:53:00 -0500

    Update 1-neuron.py

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -10,9 +10,6 @@ class Neuron:
         if (nx) < 1:
             raise ValueError("nx must be a positive integer")
         try:
-            self.W = np.random.randn(1, nx)
-            self.b = 0
-            self.A = 0
             self.nx = nx
             self.__W = np.random.randn(1, nx)
             self.__b = 0
