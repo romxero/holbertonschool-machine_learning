a8039c04bf1c6cb28a143104bc4dadf1adbb1221
_RC_ <randy.white.1986@gmail.com>
2021-11-01 11:17:43 -0500

    Create 3-neuron.py

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+#running another test 
+import numpy as np
+
+
+class Neuron:
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if (nx) < 1:
+            raise ValueError("nx must be a positive integer")
+        try:
+            self.nx = nx
+            self.__W = np.random.randn(1, nx)
+            self.__b = 0
+            self.__A = 0
+        except:
+            print("An exception occurred")
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+    # make sure to have thre variables here
+    def forward_prop(self, X):
+        if type(X) is not np.ndarray:
+            raise TypeError("Value is supposed to be an numpy dimmensional array")
+        preAct = np.matmul(self.__W, X) + self.__b #preactivation of the single neuron
+        self.__A = 1/(1 + np.exp(-preAct))
+        return self.__A
+    def cost(self, Y, A):
+        #this is also called the loss function 
+        if type(A) is not np.ndarray:
+            raise TypeError("Value X is supposed to be an numpy dimmensional array")
+        if type(Y) is not np.ndarray:
+            raise TypeError("Value Y is supposed to be an numpy dimmensional array")
+        internalVariable = -(sum(Y * np.log(A) +  np.log(1.0000001 - A)))/A.shape[1] #calculation should be ok 
+        return internalVariable #just return a value right now and fill in the variable later 
