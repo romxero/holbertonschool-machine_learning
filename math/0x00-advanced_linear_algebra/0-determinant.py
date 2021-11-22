7d0b31d0d308cc3fd52cd4e7c2d577564fdb2944
_RC_ <randy.white.1986@gmail.com>
2021-11-22 15:32:09 -0600

    changed it to take out the math ceiling function

--- math/0x00-advanced_linear_algebra/0-determinant.py
+++ math/0x00-advanced_linear_algebra/0-determinant.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-
+import numpy as np
 #use the same shebang that students are using.
-
 
 def determinant(matrix):
   if matrix:
-    import numpy as np
-    import math
     innerMatrice = np.array(matrix)
     if innerMatrice.size == 0:
       return 1
     determinantBuffer = np.linalg.det(innerMatrice)
-    return math.ceil(determinantBuffer) #this is wrong
+    return (determinantBuffer) #this is wrong
   return 1
 
