ee558409e19dbe1c5b556ea082dd06f97e7b1c27
_RC_ <randy.white.1986@gmail.com>
2021-11-23 11:56:14 -0600

    Update 0-initialize.py

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -3,13 +3,14 @@
 K-means clustering
 """
 
+import numpy as np
+
 
 # sk.cluster.KMeans
 # something somethign 
 
 
 def initialize(X, k):
-    import numpy as np 
     n,d = X.shape #get the shape of the matric
     clusters = np.random.uniform(low=np.min(X, axis=0), high=np.max(X, axis=0), size=(k,d))
     return clusters  
