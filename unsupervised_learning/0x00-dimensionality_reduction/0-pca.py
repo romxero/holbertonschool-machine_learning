ba6283e50e0398837a35f57a0eca917a7cd13252
_RC_ <randy.white.1986@gmail.com>
2021-11-23 10:49:38 -0600

    I think this is done...

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -1,3 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
+import numpy as np #import numpy in the function- not sure if check wants me to do this
+"""
+PCA analysis for dimensionality reduction 
+"""
 
-#principle component analysis. 
+
+def pca(X, var=0.95):
+    u,s, vh  = np.linalg.svd(X) #singulaur value decomposition returns u, s and vh
+    W = vh.T #W = vh.T
+    sCummulativeSum = np.cumsum(s) #sum up the singular values 
+    totalVariance = np.cumsum(s)/np.sum(s) # I know we need to get the variance somewhere here 
+    idx = np.argmax(totalVariance >= var) + 1 
+    return W[:,:idx]
